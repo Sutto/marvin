25026a31037f73572b058968895b977ba2af7534
Darcy Laycock <sutto@sutto.net>
2008-08-15 23:25:38 +0800

    Added first version of README

--- config/setup.rb
+++ config/setup.rb
@@ -2,6 +2,11 @@
 # when the clien connects.
 Marvin::Loader.before_connecting do
   
+  # E.G.
+  # MyHandler.register! (Marvin::Base subclass) or
+  # Marvin::Settings.default_client.register_handler my_handler (a handler instance)
+  
+  # Example Handler use.
   LoggingHandler.register! if Marvin::Settings.use_logging
   KnowledgeHandler.register!
   
