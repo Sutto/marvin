e497afd7fb53bfaedf5471ccdcebd37f99557b0e
Darcy Laycock <sutto@sutto.net>
2008-11-22 22:00:39 +0900

    More server stuff - add handlers to the Channel class

--- lib/marvin/irc/server/channel.rb
+++ lib/marvin/irc/server/channel.rb
@@ -1,6 +1,7 @@
 module Marvin::IRC::Server
   class Channel
-   
+    include Marvin::Dispatchable
+    
     attr_accessor :name, :members, :name, :topic, :operators, :mode
     
     def initialize(name)
@@ -9,6 +10,7 @@ module Marvin::IRC::Server
       @operators = []
       @topic     = ""
       @mode      = ""
+      dispatch :channel_created, :channel => self
     end
     
     def member?(user)
@@ -34,42 +36,60 @@ module Marvin::IRC::Server
       return false if member?(user)
       # Otherwise, we add a user
       add user
-      @member.each do |m|
-        m.notify :join, user.nick, :prefix => user.prefix
-      end
+      @member.each { |m| m.notify :join, user.nick, :prefix => user.prefix }
+      dispatch :outgoing_join, :target => @name, :nick => user.nick
       return true
     end
     
     def part(user, message = nil)
       return false if !member?(user)
-      @members.each do |m|
-        m.notify :part, @name, user.nick, message, :prefix => user.prefix
-      end
+      @members.each { |m| m.notify :part, @name, user.nick, message, :prefix => user.prefix }
       remove user
       # TODO: Remove channel from ChannelStore if it's empty.
+      dispatch :outgoing_part, :target => @name, :user => user, :message => message
+      check_emptyness!
       return true
     end
     
     def quit(user, message = nil)
       remove user
-      @members.each do |m|
-        m.notify :quit, @name, message, :prefix =>  user.prefix
-      end
+      @members.each { |m| m.notify :quit, @name, message, :prefix =>  user.prefix }
+      # TODO: Remove channel from the store if it's empty
+      dispatch :outgoing_quit, :target => @name, :user => user, :message => message
+      check_emptyness!
     end
     
-    def message(user, contents)
+    def message(user, message)
+      @members.each { |m| m.notify :privmsg, @name, message, :prefix => user.prefix unless user == m }
+      dispatch :outgoing_message, :target => @name, :user => user, :message => message
     end
     
     def notice(user, message)
+      @members.each { |m| m.notify :notice, @name, message, :prefix => user.prefix unless user == m }
+      dispatch :outgoing_notice, :target => @name, :user => user, :message => message
     end
     
-    def topic(user, message = nil)
+    def topic(user = nil, t = nil)
+      return @topic if t.blank?
+      @topic = t
+      @members.each { |m| m.notify :topic, @name, t, :prefix => user.prefix }
+      dispatch :outgoing_topic, :target => @name, :user => user, :topic => t
+      return @topic
     end
     
     private
     
     def needs_op?
       @operators.empty? && @members.empty?
+    end
+    
+    def check_emptyness!
+      destroy if @members.empty?
+    end
+    
+    def destroy
+      Marvin::IRC::Server::ChannelStore.delete(@name)
+      dispatch :channel_destroyed, :channel => self
     end
     
   end
