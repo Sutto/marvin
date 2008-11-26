40fa9fd0c157ec959dbdbb78596989c7311936ab
Darcy Laycock <sutto@sutto.net>
2008-11-26 21:36:06 +0900

    Added first set of parser tests

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 class ParserTest < Test::Unit::TestCase
-  @@parser = Marvin::Parsers::SimpleParser
+  @@parser = Marvin::Parsers::RagelParser
   
   context "When parsing a LIST" do
     setup { @parsed = @@parser.new("LIST #twilight_zone,#42") }
