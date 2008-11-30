97999254a6135cfa79de8ac30206e6cbd58c274f
Darcy Laycock <sutto@sutto.net>
2008-12-01 04:57:48 +0900

    Client will now look for the next available ring space

--- lib/marvin/distributed/drb_client.rb
+++ lib/marvin/distributed/drb_client.rb
@@ -2,6 +2,10 @@ module Marvin
   module Distributed
     # A method for operating on marvin objects.
     class DRbClient
+      
+      # Wait 1 second on a lookup.
+      LOOKUP_TIMEOUT = 1
+      
       class << self
         
         @@handlers = []
@@ -11,6 +15,7 @@ module Marvin
         end
         
         def dispatch(name, opts, client)
+          Marvin::Logger.debug "Processing Event: #{name}"
           full_handler_name = :"handle_#{name.to_s.underscore}"
           @@handlers.each do |handler|
             has_client = handler.respond_to?(:client=)
@@ -28,19 +33,35 @@ module Marvin
           Marvin::ExceptionTracker.log(e)
         end
         
+        # Starts up a drb processor / client, and walks through the process of dealing
+        # with it / processing events.
         def run
           Marvin::Logger.info "Starting up DRb Client"
           DRb.start_service
-          rs = nil
           # Loop through, making sure we have a valid
           # RingFinger and then process events as they
           # appear.
+          enter_loop!
+        end
+        
+        def ring_server
+          @ring_server = Rinda::RingFinger.finger.lookup_ring(LOOKUP_TIMEOUT) if @ring_server.nil?
+          return @ring_server
+        rescue RingNotFound
+          @ring_server = nil
+        end
+        
+        def enter_loop!
           Marvin::Logger.info "Entering processing loop"
           loop do
-            rs ||= Rinda::RingFinger.primary
-            unless rs.blank?
-              event = rs.take([:marvin_event, nil, nil, nil])
-              dispatch(*event[1..-1])
+            begin
+              unless self.ring_server.blank?
+                event = self.ring_server.take([:marvin_event, nil, nil, nil])
+                dispatch(*event[1..-1])
+              end
+            rescue
+              # Reset the ring server on event of connection refused etc.
+              @ring_server = nil
             end
           end
         end
