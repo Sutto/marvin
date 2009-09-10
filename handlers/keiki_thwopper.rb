cc1b45e63a4f1938b9662e4dbf94debefc754443
Darcy Laycock <sutto@sutto.net>
2009-09-11 01:23:53 +0800

    Fix KeikiThwopper

--- handlers/keiki_thwopper.rb
+++ handlers/keiki_thwopper.rb
@@ -1,7 +1,7 @@
 class KeikiThwopper < Marvin::Base
   
   FROM_REGEXP  = /^(keiki)/i
-  THWOP_REGEXP = /(t+h+w+o+p+|stabs|kills)/i
+  THWOP_REGEXP = /(t+h+w+o+p+|stab|kill)/i
   
   MESSAGES = [
     "mwahahahaha",
@@ -10,13 +10,12 @@ class KeikiThwopper < Marvin::Base
     "say hello to my little friend",
     "you know, they could have liked that?"
   ]
-  za
+  
   on_event :incoming_action,  :thwop_back
   
   def thwop_back
-    return if !from || from !~ FROM_REGEXP
-    return if options.message !~ THWOP_REGEXP
-    action "#{$1} #{from} (#{MESSAGES[rand(MESSAGES.length)]})"
+    return if !from || from !~ FROM_REGEXP || options.message !~ THWOP_REGEXP
+    action "#{$1}s #{from} (#{MESSAGES[rand(MESSAGES.length)]})"
   end
   
 end
